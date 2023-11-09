575b752611077a13fc4476bc6d465092cfaf4286
rmoreno <gecko_2321@hotmail.com>
2023-11-09 00:29:19 -0300

    Pre entrega 2 JS

--- js/main.js
+++ js/main.js
@@ -11,28 +11,28 @@ function usuario() {
   alert("El usuario ingresado es " + user); //muestro el usuario
 }
 
-//Funcion para buscar el precio del producto (entre 3 productos)
-function buscarp(letra) {
-  switch (letra) {
-    case "A":
-      return 3500;
-      break;
-    case "B":
-      return 4500;
-      break;
-    case "C":
-      return 5500;
-      break;
-    default:
-      0;
-      break;
-  }
-}
+// //Funcion para buscar el precio del producto (entre 3 productos)
+// function buscarp(letra) {
+//   switch (letra) {
+//     case "A":
+//       return 3500;
+//       break;
+//     case "B":
+//       return 4500;
+//       break;
+//     case "C":
+//       return 5500;
+//       break;
+//     default:
+//       0;
+//       break;
+//   }
+// }
 
-//funcion para sumar montos de ambos items para el carrito
-function sumar(primerItem, segundoItem) {
-  return primerItem + segundoItem;
-}
+// //funcion para sumar montos de ambos items para el carrito
+// function sumar(primerItem, segundoItem) {
+//   return primerItem + segundoItem;
+// }
 
 //llamo a la funcion usuario
 usuario();
@@ -56,13 +56,115 @@ if (user !== "richard") {
 
 //Ingreso al sistema
 
-//pido que indique el producto 1 y 2 a agregar al carrito
-let letra1 = prompt("Ingrese Item 1 a Agregar al carrito").toUpperCase();
-let letra2 = prompt("Ingrese Item 2 a Agregar al carrito").toUpperCase();
+// //pido que indique el producto 1 y 2 a agregar al carrito
+// let letra1 = prompt("Ingrese Item 1 a Agregar al carrito").toUpperCase();
+// let letra2 = prompt("Ingrese Item 2 a Agregar al carrito").toUpperCase();
 
-//asigno el valor de cada producto a una variable 
-let item1 = buscarp(letra1);
-let item2 = buscarp(letra2);
+// //asigno el valor de cada producto a una variable
+// let item1 = buscarp(letra1);
+// let item2 = buscarp(letra2);
 
-//muestro el total de ambos items
-console.log("El monto del carrito es " + sumar(item1, item2));
+// //muestro el total de ambos items
+// console.log("El monto del carrito es " + sumar(item1, item2));
+
+//Creo array de productos que se corresponden a los productos publicados en la pagina
+const productos = [
+  {
+    id: 1,
+    nombre: "Taza + Plato",
+    descripcion: "Fabricados con ceramica multicolor",
+    precio: 700,
+  },
+  {
+    id: 2,
+    nombre: "Panera",
+    descripcion: "Gran capacidad y dureza",
+    precio: 800,
+  },
+  {
+    id: 3,
+    nombre: "Compotera",
+    descripcion: "Polifuncional, tinturas multicolor",
+    precio: 650,
+  },
+  {
+    id: 4,
+    nombre: "Juego de Tazas",
+    descripcion: "6 Tazas multicolor",
+    precio: 1150,
+  },
+  {
+    id: 5,
+    nombre: "Combo Taza y recipiente",
+    descripcion: "Taza + azucarera",
+    precio: 850,
+  },
+  {
+    id: 6,
+    nombre: "Compotera alta",
+    descripcion: "Compotera para altas temperaturas",
+    precio: 1300,
+  },
+  {
+    id: 7,
+    nombre: "Taza + Plato Color Ocre",
+    descripcion: "Ideal para desayunos",
+    precio: 1000,
+  },
+  {
+    id: 8,
+    nombre: "Juego de Tazas + Plato",
+    descripcion: "Juego de Tazas + Plato multicolores",
+    precio: 1600,
+  },
+  {
+    id: 9,
+    nombre: "Tazon Sopero",
+    descripcion: "Tazon Sopero de gran capacidad",
+    precio: 1200,
+  },
+];
+
+//funcion para recorrer el array
+function mostrarProductos(productos) {
+  productos.forEach((producto) =>
+    console.log(
+      producto.nombre + " - " + producto.descripcion + " - " + producto.precio
+    )
+  );
+}
+
+//funcion para filtrado segun input
+function filtrarProductos() {
+  const resultado = productos
+  .filter(filtrarNombre)
+  .filter(filtrarPrecio);
+  if (resultado.length > 0) {
+    mostrarProductos(resultado);
+  } else {
+    alert("No hay coincidencia");
+  }
+}
+
+//funcion filtro por nombre
+function filtrarNombre(producto) {
+  if (nombre) {
+    return producto.nombre === nombre;
+  }
+  return producto;
+}
+
+//funcion filtro por precio
+function filtrarPrecio(producto) {
+  if (precio) {
+    return producto.precio === precio;
+  }
+  return producto;
+}
+
+//solicito Nombre de producto y precio
+let nombre = prompt("Ingresar Producto a buscar");
+let precio = Number(prompt("Ingresar precio de preferencia"));
+
+//ejecuto funcion
+filtrarProductos();
